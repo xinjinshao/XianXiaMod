54874c4b87c41e5931fb54ecbbcba8959aa739b7
xinjinshao <394726202@qq.com>
2026-06-12 08:03:30 +0400

    Wire debug drops config

---
+++ Common/Systems/DebugDropGlobalNPC.cs
@@ -0,0 +1,33 @@
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using XianXia.Content.Items.Generated;
+using XianXia.Content.Items.Materials;
+
+namespace XianXia.Common.Systems;
+
+public class DebugDropGlobalNPC : GlobalNPC
+{
+    public override void OnKill(NPC npc)
+    {
+        if (Main.netMode == NetmodeID.MultiplayerClient)
+        {
+            return;
+        }
+
+        if (!ModContent.GetInstance<XianXiaConfig>().DebugDrops || npc.ModNPC?.Mod != Mod)
+        {
+            return;
+        }
+
+        IEntitySource source = npc.GetSource_Loot();
+        int spiritStoneStack = npc.boss ? 25 : Main.rand.Next(2, 5);
+        Item.NewItem(source, npc.Hitbox, ModContent.ItemType<LowGradeSpiritStone>(), spiritStoneStack);
+
+        if (npc.boss)
+        {
+            Item.NewItem(source, npc.Hitbox, ModContent.ItemType<ArtifactBlankShard>(), 3);
+        }
+    }
+}
